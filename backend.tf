9fb24743dcb1ffd9658a78dfa0947a6b884799fa
Sunday Ayandele <deaconsunday@gmail.com>
2021-05-12 08:12:48 +0200

    updated backend.tf
    awsks

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket     = "myterraformfiles"
     region     = "us-east-1"
     key        = "remote-state-store-terraform.tfstate"
-    access_key = "AKIASVSHMXHQ7UIDNYXG"
-    secret_key = "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"
+    access_key = "xxxxxxxxx"
+    secret_key = "xxxxxxxx"
   }
 }
